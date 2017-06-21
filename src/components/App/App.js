aef79e26d8a2dfa3abdf13e70ec02e238370220a
Nima Mehanian <nima.mehanian@gmail.com>
2017-06-21 02:42:48 -0700

    Add Spinner component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from '../Header';
 import Group from '../Group';
+import Spinner from '../Spinner';
 
 class App extends Component {
   constructor(props) {
@@ -9,34 +10,44 @@ class App extends Component {
     this.gather = this.gather.bind(this);
   }
 
+  componentDidMount() {
+    this.props.load();
+  }
+
   gather() {
-    this.props.gather(this.props.teams);
+    console.log(this.props.teams);
+    // this.props.gather(this.props.teams);
   }
 
   render() {
     return (
-      <div className="app">
-        <Header />
-        <div className="groups">
-          {this.props.lunchGroups.map((group, index) => (
-            <Group group={group} key={`group_${index + 1}`} />
-          ))}
+      <div>
+        {!this.props.isDataLoaded ?
+          <div className="app-loading"><Spinner /></div> : null
+        }
+        <div className="app">
+          <Header />
+          <div className="groups">
+            {this.props.lunchGroups.map((group, index) => (
+              <Group group={group} key={`group_${index + 1}`} />
+            ))}
+          </div>
+          <div className="feather-btm" />
+          <button
+            disabled={this.props.groupsAreResolved}
+            className="gather"
+            onClick={this.gather}
+            style={{
+              cursor: this.props.groupsAreResolved ? 'not-allowed' : 'pointer',
+              opacity: this.props.groupsAreResolved ? 0.5 : 1,
+              transition: 'opacity 0.1s cubic-bezier(0.165, 0.84, 0.44, 1)',
+            }}
+          >
+            <span className="left" role="img" aria-label="rockstar">👊🏼✨</span>
+            let&apos;s roll
+            <span className="right" role="img" aria-label="cheers">🍣🥂</span>
+          </button>
         </div>
-        <div className="feather-btm" />
-        <button
-          disabled={this.props.groupsAreResolved}
-          className="gather"
-          onClick={this.gather}
-          style={{
-            cursor: this.props.groupsAreResolved ? 'not-allowed' : 'pointer',
-            opacity: this.props.groupsAreResolved ? 0.5 : 1,
-            transition: 'opacity 0.1s cubic-bezier(0.165, 0.84, 0.44, 1)',
-          }}
-        >
-          <span className="left" role="img" aria-label="rockstar">👊🏼✨</span>
-          let&apos;s roll
-          <span className="right" role="img" aria-label="cheers">🍣🥂</span>
-        </button>
       </div>
     );
   }
